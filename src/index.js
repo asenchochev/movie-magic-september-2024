c0e9f46690a9e1d3f4dcb16aac8144ab34416704
Asen Chochev <asen.qkiqt145@gmail.com>
2024-09-22 17:03:54 +0300

    Setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import express from 'express';
+
+const app = express();
+
+    app.get('/', (req, res) => {
+        res.send('I think it works');
+    });
+
+app.listen(5000);
+console.log("Server is listening on http://localhost:5000...");
