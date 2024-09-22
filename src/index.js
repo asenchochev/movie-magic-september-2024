4f86af4d2f4f7ec8d1b69cb3dc29cc3ecf09f864
Asen Chochev <asen.qkiqt145@gmail.com>
2024-09-22 18:03:06 +0300

    Render home Page

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ const app = express();
     
     app.set('view engine', 'hbs')
     app.set('views', 'src/views');
+    
+    app.use(express.static('public'));
 
     app.get('/', (req, res) => {
-        res.render('index');
+        res.render('home');
     });
 
 app.listen(5000);
